47054b836f1df6f65f8578095279c29bafaeaeb2
stefano <stefano@futureworkshops.com>
2015-02-24 11:14:01 +0000

    remove logs

--- crc/src/main/java/net/rehacktive/crc/Utils.java
+++ crc/src/main/java/net/rehacktive/crc/Utils.java
@@ -5,7 +5,7 @@ import android.util.Log;
 
 public class Utils {
 	
-	private static boolean isLogEnabled = true;
+	private static boolean isLogEnabled = false;
 	
 	public static void log(String tag, String content) {
 		if(isLogEnabled) Log.d(tag,content);
